f136fccbd5df2a92d2b0c7acad4e889a2562f7e6
Joowon-Seo <tjwndnjs1998@gmail.com>
2025-05-12 14:54:26 +0900

    refactor : apiConfig 수정

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,8 +1,12 @@
 // API URL 설정
 export const API_BASE_URL =
-  process.env.NODE_ENV === "production"
+  process.env.REACT_APP_API_URL ||
+  (process.env.NODE_ENV === "production"
     ? "https://eum.o-r.kr/api/v1"
-    : "http://localhost:8080/api/v1";
+    : "https://eum.o-r.kr/api/v1");
+
+console.log("Current API URL:", API_BASE_URL);
+console.log("Current NODE_ENV:", process.env.NODE_ENV);
 
 // API 호출을 위한 기본 함수 (인증 토큰 자동 포함)
 export const fetchAPI = async (endpoint: string, options: RequestInit = {}) => {
